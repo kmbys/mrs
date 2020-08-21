b74901182f11add4f9adb55a2e74d73cb9046787
nsbt <55523468+nsbt@users.noreply.github.com>
2020-08-21 22:03:40 +0900

    create rooms handler

---
+++ src/main/kotlin/com/nsbt/mrs/app/room/RoomsController.kt
@@ -0,0 +1,21 @@
+package com.nsbt.mrs.app.room
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
+import java.time.LocalDate
+
+@Controller
+@RequestMapping("rooms")
+class RoomsController {
+
+    @GetMapping
+    fun listRooms(): ModelAndView =
+        ModelAndView("room/listRooms",
+            mapOf(
+                "date" to LocalDate.now(),
+                "room" to "会議室"
+            )
+        )
+}
