b2c752fed7681520e31b253654cc815f091f869e
nsbt <55523468+nsbt@users.noreply.github.com>
2020-08-21 20:56:13 +0900

    first commit

---
+++ src/test/kotlin/com/nsbt/mrs/MrsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nsbt.mrs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MrsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
