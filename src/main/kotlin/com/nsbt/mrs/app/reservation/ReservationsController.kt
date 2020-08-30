2bedeef7aba19a6313c9ea3570e6c9c25fae0f9d
nsbt <55523468+nsbt@users.noreply.github.com>
2020-08-31 08:06:47 +0900

    create post /reservations/{date}/{roomId} handler

--- src/main/kotlin/com/nsbt/mrs/app/reservation/ReservationsController.kt
+++ src/main/kotlin/com/nsbt/mrs/app/reservation/ReservationsController.kt
@@ -7,8 +7,10 @@ import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.servlet.View
 import java.time.LocalDate
 import java.time.LocalTime
 
@@ -35,6 +37,13 @@ class ReservationsController(
                 "user" to dummyUser()
             )
         )
+
+    @PostMapping
+    fun reserve(
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable date: LocalDate,
+        @PathVariable roomId: Int
+    ) =
+        ModelAndView("redirect:/reservations/{date}/{roomId}")
 
     private fun timeList() =
         (0 until 24 * 2).map { LocalTime.of(it / 2, it % 2 * 30) }
