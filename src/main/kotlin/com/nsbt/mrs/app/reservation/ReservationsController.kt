50295103d50339fd653a51124ad3310ace4b36d6
nsbt <55523468+nsbt@users.noreply.github.com>
2020-09-17 17:20:34 +0900

    fix reservation logic

--- src/main/kotlin/com/nsbt/mrs/app/reservation/ReservationsController.kt
+++ src/main/kotlin/com/nsbt/mrs/app/reservation/ReservationsController.kt
@@ -64,7 +64,7 @@ class ReservationsController(
             ReservableRoom(
                 ReservableRoomId(
                     roomId,
-                    LocalDate.now()
+                    date
                 ),
                 roomService.findMeetingRoom(roomId)
             ),
